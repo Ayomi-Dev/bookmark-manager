f78ceece73c6118ee6bd54ca445ae5e44c379b2f
Ayomide <ayomideolakunlea1@gmail.com>
2026-01-28 16:03:17 +0100

    Fixed how api route is called in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
       });
 
     try {
-      const res = await fetch("http://localhost:3000/api/auth/login", {
+      const res = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "applcation/json"
